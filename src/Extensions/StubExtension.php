345067714f078ed0c5d1c56a523338e1a5900aa5
Noël Hagestein <6616996+Neol3108@users.noreply.github.com>
2024-06-16 20:10:41 +0200

    fix: static analysis

--- src/Extensions/StubExtension.php
+++ src/Extensions/StubExtension.php
@@ -17,6 +17,12 @@ final class StubExtension implements StubFilesExtension
     {
         return [
             __DIR__ . '/../../stubs/contracts/database/eloquent/builder.stub',
+            __DIR__ . '/../../stubs/contracts/database/query/builder.stub',
+            __DIR__ . '/../../stubs/contracts/database/query/expression.stub',
+            __DIR__ . '/../../stubs/contracts/queue/queueable-collection.stub',
+            __DIR__ . '/../../stubs/contracts/support/arrayable.stub',
+            __DIR__ . '/../../stubs/contracts/support/can-be-escaped-when-cast-to-string.stub',
+            __DIR__ . '/../../stubs/contracts/support/jsonable.stub',
             __DIR__ . '/../../stubs/database/eloquent/concerns/queries-relationships.stub',
             __DIR__ . '/../../stubs/database/eloquent/relations/belongs-to.stub',
             __DIR__ . '/../../stubs/database/eloquent/relations/belongs-to-many.stub',
@@ -25,7 +31,15 @@ final class StubExtension implements StubFilesExtension
             __DIR__ . '/../../stubs/database/eloquent/relations/has-one-or-many.stub',
             __DIR__ . '/../../stubs/database/eloquent/relations/relation.stub',
             __DIR__ . '/../../stubs/database/eloquent/builder.stub',
+            __DIR__ . '/../../stubs/database/eloquent/collection.stub',
             __DIR__ . '/../../stubs/database/eloquent/model.stub',
+            __DIR__ . '/../../stubs/database/eloquent/model-not-found-exception.stub',
+            __DIR__ . '/../../stubs/database/query/builder.stub',
+            __DIR__ . '/../../stubs/database/multiple-records-found-exception.stub',
+            __DIR__ . '/../../stubs/database/records-not-found-exception.stub',
+            __DIR__ . '/../../stubs/support/collection.stub',
+            __DIR__ . '/../../stubs/support/enumerable.stub',
+            __DIR__ . '/../../stubs/support/lazy-collection.stub',
         ];
     }
 }
